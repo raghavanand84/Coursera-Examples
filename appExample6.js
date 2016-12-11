ce2d4ff537660fd66c52b0ffc7086589ef3156c6
Raghav Anand <Raghav Anand>
2016-12-12 00:14:52 +0530

    Exmaple 6

---
+++ appExample6.js
@@ -0,0 +1,24 @@
+(function(){
+  'use strict';
+
+angular.module("NameCalculator",[])
+.controller("NameCalculatorController",function($scope){
+      $scope.name = "Raghav";
+      $scope.totolValue = 0;
+
+      $scope.displayNumeric = function(){
+        var totalNameValue = calculateNumericForString($scope.name);
+        $scope.totolValue = totalNameValue;
+      }
+
+      var calculateNumericForString = function(string){
+        var totalStringValue = 0;
+        for(var i = 0;i<string.length;i++)
+        {
+          totalStringValue += string.charCodeAt(i);
+        }
+        return totalStringValue;
+      }
+});
+
+})();
